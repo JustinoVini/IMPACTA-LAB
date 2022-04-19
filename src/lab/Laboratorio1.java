6fdfee2c6fd9ba379bf278a3c62d35ade74c8cf7
Vinicius Justino <visilva5690@hotmail.com>
2022-04-19 15:24:58 -0300

    first commit

---
+++ src/lab/Laboratorio1.java
@@ -0,0 +1,11 @@
+package lab;
+
+public class Laboratorio1 {
+	public static void main(String[] args) {
+		
+		int valor1 = 10, valor2 = 20;
+		int resultado = valor1 + valor2;
+		System.out.println("10 + 20 = " + resultado);		
+
+	}
+}
---
+++ src/lab/Laboratorio2.java
@@ -0,0 +1,11 @@
+package lab;
+
+public class Laboratorio2 {
+	
+	public static void main(String[] args) {
+		
+		int alpha = 20, betha = 40;
+		
+		System.out.println(alpha * betha > betha - alpha ? "Sim, é maior!" : "Não, não é maior!");
+	}
+}
